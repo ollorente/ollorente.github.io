f3838dcfa76740dd4febc7d0338b4d7959bfa711
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-16 14:52:08 -0500

    Estructura base

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Acerca de</h1>
+  </main>
+</template>
