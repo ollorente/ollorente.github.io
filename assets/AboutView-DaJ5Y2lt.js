43396445c972293c4a4f3113eed6423c5f7ff62a
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-16 14:57:21 -0500

    Updates

---
+++ assets/AboutView-DaJ5Y2lt.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o,a as t}from"./index-PH66mA2E.js";const n={},s=t("h1",null,"Acerca de",-1),a=[s];function r(_,l){return o(),c("main",null,a)}const i=e(n,[["render",r]]);export{i as default};
---
+++ assets/HomeView-H_V_Vsrl.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c,a as t}from"./index-PH66mA2E.js";const n={},s=t("h1",null,"Home",-1),a=[s];function _(r,l){return c(),o("main",null,a)}const m=e(n,[["render",_]]);export{m as default};
